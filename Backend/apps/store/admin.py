f7c792bac26b7bca699dfae132283760dda822e3
MamesJikolai <jamesmikolaisalazar@gmail.com>
2026-04-19 18:05:59 +0800

    feat(admin): register store models in django admin interface
    add admin configurations for Product, Workshop, Order, OrderItem, Booking, and Newsletter models

--- Backend/apps/store/admin.py
+++ Backend/apps/store/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Product, Workshop, OrderItem, Booking, Order, Newsletter
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'quantity', 'is_active')
+
+
+@admin.register(Workshop)
+class WorkshopAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'date', 'start_time', 'capacity')
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('name','email', 'total_amount', 'is_paid', 'created_at')
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('order', 'product', 'quantity')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('order', 'workshop', 'seats_reserved')
+
+
+@admin.register(Newsletter)
+class NewsletterAdmin(admin.ModelAdmin):
+    list_display = ('email', 'date_subscribed')
