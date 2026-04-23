8fcda8da826dd2b0209fc4a8379a5934896c4e95
MamesJikolai <jamesmikolaisalazar@gmail.com>
2026-04-23 23:45:30 +0800

    feat(workshop): add basic workshop page content section

--- Frontend/src/pages/Workshop.tsx
+++ Frontend/src/pages/Workshop.tsx
@@ -8,6 +8,12 @@ export default function Workshop() {
                 title="Workshop"
                 backgroundImage="/img/workshop.jpg"
             />
+
+            <div className="flex flex-col gap-8 md:gap-16 items-center w-full h-fit px-2.5 py-10 md:p-32">
+                <h2>Schedule Your Workshop</h2>
+
+                <div></div>
+            </div>
         </div>
     )
 }
